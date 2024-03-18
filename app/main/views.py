3e11c35b34c7a1c7fadedc7488f7bbba01f82322
Geri <waldmanngeri@gmail.com>
2024-03-18 21:20:27 +0100

    posts display in user/index.html

--- app/main/views.py
+++ app/main/views.py
@@ -2,16 +2,25 @@ import os
 from flask import render_template, session, redirect, url_for, current_app, flash
 from flask_login import login_required, current_user
 from .. import db
-from ..models import User
-from ..email import send_email
 from . import main
 from .forms import NameForm
-from ..models import Role, User
-from .forms import EditProfileForm, EditProfileAdminForm
 from ..decorators import admin_required
+from ..models import Permission, Role, User, Post
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from datetime import datetime
 import smtplib
 
+@main.route('/', methods=['GET', 'POST'])
+def posts():
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,7 +53,8 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
