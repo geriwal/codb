42eb1de90c139a9654d15d5bb23339a0ba9df7c7
Geri <waldmanngeri@gmail.com>
2024-03-19 21:03:58 +0100

    edit post

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import request, render_template, session, redirect, url_for, current_app, flash
+from flask import request, render_template, session, redirect, url_for, current_app, flash, abort
 from flask_login import login_required, current_user
 from .. import db
 from . import main
@@ -9,6 +9,7 @@ from ..models import Permission, Role, User, Post
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from datetime import datetime
 import smtplib
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -111,3 +112,20 @@ def edit_profile_admin(id):
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMIN):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
